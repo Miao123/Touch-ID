4d0d25f8124cce4ab8eb1ded43dcee68bfa00eeb
Miao123 <andy@andytekiAir.lan>
2015-07-21 14:23:19 +0800

    2222
    22222

--- Touch ID解锁/ViewController.m
+++ Touch ID解锁/ViewController.m
@@ -1,13 +1,13 @@
 //
 //  ViewController.m
-//  Touch ID解锁
+//  网页
 //
-//  Created by andy on 15/7/21.
-//  Copyright (c) 2015年 andy. All rights reserved.
+//  Created by mac on 15-7-18.
+//  Copyright (c) 2015年 mac. All rights reserved.
 //
 
 #import "ViewController.h"
-
+#import <LocalAuthentication/LocalAuthentication.h>
 @interface ViewController ()
 
 @end
@@ -17,7 +17,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    LAContext *myContext = [[LAContext alloc] init];
+    NSError *authError = nil;
+    NSString *myLocalized = @"请继续扫描你的指纹";
+    if ([myContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&authError]) {
+        [myContext evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:myLocalized reply:^(BOOL success, NSError *error) {
+            if (success) {
+                NSLog(@"成功");
+                if (!success) {
+                    NSLog(@"%@",error);
+                }
+            }else {
+                NSLog(@"authentication failed");
+                if (!success) {
+                    NSLog(@"%@",error);
+                }
+            }
+        }];
+    }
+    //    else{
+    //        NSLog(@"发生一个错误");
+    //        if(!success){
+    //            NSLog(@"%@",error);
+    //        }
+    //    }
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
